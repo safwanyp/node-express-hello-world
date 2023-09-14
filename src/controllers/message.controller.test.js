8873824d12edd99110efcf1cb4d0d2a15f6a7c31
Safwan Parkar <safwanparkar6@gmail.com>
2023-09-14 09:39:18 +0400

    test: add error cases to message controller

--- src/controllers/message.controller.test.js
+++ src/controllers/message.controller.test.js
@@ -18,125 +18,103 @@ const messageService = require("../services/message.service.js");
 jest.mock("../services/message.service.js");
 
 describe("message.controller", () => {
-    describe("getMessageById", () => {
-        it("should return a message by id", async () => {
-            const mockRequest = {
-                params: {
-                    id: 1,
-                },
-            };
+    describe("Happy cases", () => {
+        describe("getMessageById", () => {
+            it("should return a message by id", async () => {
+                const mockRequest = {
+                    params: {
+                        id: 1,
+                    },
+                };
 
-            const mockResponse = {
-                status: jest.fn().mockReturnThis(),
-                json: jest.fn(),
-            };
+                const mockResponse = {
+                    status: jest.fn().mockReturnThis(),
+                    json: jest.fn(),
+                };
 
-            const mockNext = jest.fn();
+                const mockNext = jest.fn();
 
-            messageService.getMessageById.mockResolvedValue({
-                id: 1,
-                created_by: "safwan",
-                message: "hello",
-            });
-
-            await messageController.getMessageById(
-                mockRequest,
-                mockResponse,
-                mockNext,
-            );
-
-            expect(messageService.getMessageById).toHaveBeenCalledWith(1);
-            expect(mockNext).toHaveBeenCalledWith({
-                code: StatusCodes.OK,
-                status: "Success",
-                message: {
+                messageService.getMessageById.mockResolvedValue({
                     id: 1,
                     created_by: "safwan",
                     message: "hello",
-                },
+                });
+
+                await messageController.getMessageById(
+                    mockRequest,
+                    mockResponse,
+                    mockNext,
+                );
+
+                expect(messageService.getMessageById).toHaveBeenCalledWith(1);
+                expect(mockNext).toHaveBeenCalledWith({
+                    code: StatusCodes.OK,
+                    status: "Success",
+                    message: {
+                        id: 1,
+                        created_by: "safwan",
+                        message: "hello",
+                    },
+                });
             });
         });
-    });
 
-    describe("createMessage", () => {
-        it("should create a message", async () => {
-            const mockRequest = {
-                body: {
-                    created_by: "safwan",
-                    message: "hello",
-                },
-            };
+        describe("createMessage", () => {
+            it("should create a message", async () => {
+                const mockRequest = {
+                    body: {
+                        created_by: "safwan",
+                        message: "hello",
+                    },
+                };
 
-            const mockResponse = {
-                status: jest.fn().mockReturnThis(),
-                json: jest.fn(),
-            };
+                const mockResponse = {
+                    status: jest.fn().mockReturnThis(),
+                    json: jest.fn(),
+                };
 
-            const mockNext = jest.fn();
+                const mockNext = jest.fn();
 
-            messageService.createMessage.mockResolvedValue({
-                id: 1,
-                created_by: "safwan",
-                message: "hello",
-            });
-
-            await messageController.createMessage(
-                mockRequest,
-                mockResponse,
-                mockNext,
-            );
-
-            expect(messageService.createMessage).toHaveBeenCalledWith({
-                created_by: "safwan",
-                message: "hello",
-            });
-            expect(mockNext).toHaveBeenCalledWith({
-                code: StatusCodes.CREATED,
-                status: "Success",
-                message: {
+                messageService.createMessage.mockResolvedValue({
                     id: 1,
                     created_by: "safwan",
                     message: "hello",
-                },
+                });
+
+                await messageController.createMessage(
+                    mockRequest,
+                    mockResponse,
+                    mockNext,
+                );
+
+                expect(messageService.createMessage).toHaveBeenCalledWith({
+                    created_by: "safwan",
+                    message: "hello",
+                });
+                expect(mockNext).toHaveBeenCalledWith({
+                    code: StatusCodes.CREATED,
+                    status: "Success",
+                    message: {
+                        id: 1,
+                        created_by: "safwan",
+                        message: "hello",
+                    },
+                });
             });
         });
-    });
 
-    describe("getMessages", () => {
-        it("should return all messages", async () => {
-            const mockRequest = {};
+        describe("getMessages", () => {
+            it("should return all messages", async () => {
+                const mockRequest = {};
 
-            const mockResponse = {
-                status: jest.fn().mockReturnThis(),
-                json: jest.fn(),
-            };
+                const mockResponse = {
+                    status: jest.fn().mockReturnThis(),
+                    json: jest.fn(),
+                };
 
-            const mockNext = jest.fn();
+                const mockNext = jest.fn();
 
-            messageService.getMessages.mockResolvedValue([
-                {
-                    id: 1,
-                    created_by: "safwan",
-                    message: "hello",
-                },
-                {
-                    id: 2,
-                    created_by: "safwan",
-                    message: "hello",
-                },
-            ]);
-
-            await messageController.getMessages(
-                mockRequest,
-                mockResponse,
-                mockNext,
-            );
-
-            expect(messageService.getMessages).toHaveBeenCalled();
-            expect(mockNext).toHaveBeenCalledWith({
-                code: StatusCodes.OK,
-                status: "Success",
-                message: [
+                messageService.getMessages.mockResolvedValue([
                     {
                         id: 1,
                         created_by: "safwan",
@@ -147,7 +125,100 @@ describe("message.controller", () => {
                         created_by: "safwan",
                         message: "hello",
                     },
-                ],
+                ]);
+
+                await messageController.getMessages(
+                    mockRequest,
+                    mockResponse,
+                    mockNext,
+                );
+
+                expect(messageService.getMessages).toHaveBeenCalled();
+                expect(mockNext).toHaveBeenCalledWith({
+                    code: StatusCodes.OK,
+                    status: "Success",
+                    message: [
+                        {
+                            id: 1,
+                            created_by: "safwan",
+                            message: "hello",
+                        },
+                        {
+                            id: 2,
+                            created_by: "safwan",
+                            message: "hello",
+                        },
+                    ],
+                });
+            });
+        });
+    });
+    describe("Error cases", () => {
+        describe("getMessageById", () => {
+            it("should handle error when fetching a message by id", async () => {
+                const mockRequest = {
+                    params: {
+                        id: 1,
+                    },
+                    body: {
+                        created_by: "safwan",
+                        message: "hello",
+                    },
+                };
+
+                const mockResponse = {};
+                const mockNext = jest.fn();
+
+                // Mock the service to throw an error
+                messageService.getMessageById.mockRejectedValue({
+                    code: 400,
+                    status: "Error",
+                    message: "Request body should be empty",
+                });
+
+                await messageController.getMessageById(
+                    mockRequest,
+                    mockResponse,
+                    mockNext,
+                );
+
+                expect(mockNext).toHaveBeenCalledWith({
+                    code: StatusCodes.INTERNAL_SERVER_ERROR,
+                });
+            });
+        });
+
+        describe("getMessages", () => {
+            it("should handle error when fetching all messages", async () => {
+                const mockRequest = {
+                    params: {
+                        id: 1,
+                    },
+                    body: {
+                        created_by: "safwan",
+                        message: "hello",
+                    },
+                };
+
+                const mockResponse = {};
+                const mockNext = jest.fn();
+
+                // Mock the service to throw an error
+                messageService.getMessages.mockRejectedValue({
+                    code: 400,
+                    status: "Error",
+                    message: "Request body should be empty",
+                });
+
+                await messageController.getMessages(
+                    mockRequest,
+                    mockResponse,
+                    mockNext,
+                );
+
+                expect(mockNext).toHaveBeenCalledWith({
+                    code: StatusCodes.INTERNAL_SERVER_ERROR,
+                });
             });
         });
     });
