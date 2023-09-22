b1d393c5727dab4980f73f7eece68f0e86213028
Safwan Parkar <safwanparkar6@gmail.com>
2023-09-22 15:58:16 +0400

    chore: remove tracingId

--- src/middlewares/id.middleware.js
+++ src/middlewares/id.middleware.js
@@ -2,7 +2,6 @@ const { v4: uuidv4 } = require("uuid");
 
 function requestIdMiddleware(req, res, next) {
     req.requestId = uuidv4();
-    req.tracingId = uuidv4();
 
     res.setHeader("X-Request-ID", req.requestId);
 
